fa47f97793de0a5ea9809cb613b674230b715d61
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2020-11-29 12:53:06 +0300

    default string length (2)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider {
     /**
